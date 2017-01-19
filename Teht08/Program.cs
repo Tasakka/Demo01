cf67aa4091b41dacb187a5c7137e78f491ce880b
k3236 <k3236@student.jamk.fi>
2017-01-19 10:47:21 +0200

    asd

---
+++ Teht08/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.Write("Give three whole numbers > ");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            num3 = int.Parse(Console.ReadLine());
+            if (num1 > num2 && num1 > num2)
+            {
+                Console.WriteLine(num1);
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine(num2);
+            }
+            else if (num3 > num1 && num3 > num2)
+            {
+                Console.WriteLine(num3);
+            }
+        }
+    }
+}
