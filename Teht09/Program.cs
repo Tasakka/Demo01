c289004bd8581d270835ab21fb5d58842d0c16bb
k3236 <k3236@student.jamk.fi>
2017-01-19 11:01:15 +0200

    jygf

---
+++ Teht09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku;
+            Console.Write("Anna luku > ");
+            luku = int.Parse(Console.ReadLine());
+            while (luku != 0)
+            {
+                Console.Write("Anna luku > ");
+            }
+        }
+    }
+}
